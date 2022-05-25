69ae848294969440a85df41e18179b7e79481c45
ictapik <wahyu.hidayat@adyawinsa.com>
2022-05-25 13:58:34 +0700

    Perubahan dashboard menjadi 2 box

--- application/views/employee_v2/v_dashboard_v2.php
+++ application/views/employee_v2/v_dashboard_v2.php
@@ -52,37 +52,6 @@
             <!-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
           </div>
         </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-warning">
-            <div class="inner">
-              <h4>44</h4>
-
-              <p>User Registrations</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <!-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-danger">
-            <div class="inner">
-              <h4>65</h4>
-
-              <p>Unique Visitors</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
-            </div>
-            <!-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
-          </div>
-        </div>
-        <!-- ./col -->
       </div>
       <!-- /.row -->
     </div><!-- /.container-fluid -->
