a914f08a9f20b3420672153db9776b6b32094680
ictapik <wahyu.hidayat@adyawinsa.com>
2022-06-13 10:23:36 +0700

    Attendnace: hilangkan tombol print

--- application/views/employee/v_attendance.php
+++ application/views/employee/v_attendance.php
@@ -122,26 +122,28 @@
         searchPlaceholder: "Search...",
       },
       buttons: [{
-        extend: 'copyHtml5',
-        text: '<i class="fa fa-copy"></i>',
-        titleAttr: 'Copy'
-      }, {
-        extend: 'csvHtml5',
-        text: '<i class="fa fa-file-csv"></i>',
-        titleAttr: 'CSV'
-      }, {
-        extend: 'excelHtml5',
-        text: '<i class="fa fa-file-excel"></i>',
-        titleAttr: 'Excel'
-      }, {
-        extend: 'pdfHtml5',
-        text: '<i class="fa fa-file-pdf"></i>',
-        titleAttr: 'PDF'
-      }, {
-        extend: 'print',
-        text: '<i class="fa fa-print"></i>',
-        titleAttr: 'Print'
-      }, ],
+          extend: 'copyHtml5',
+          text: '<i class="fa fa-copy"></i>',
+          titleAttr: 'Copy'
+        }, {
+          extend: 'csvHtml5',
+          text: '<i class="fa fa-file-csv"></i>',
+          titleAttr: 'CSV'
+        }, {
+          extend: 'excelHtml5',
+          text: '<i class="fa fa-file-excel"></i>',
+          titleAttr: 'Excel'
+        }, {
+          extend: 'pdfHtml5',
+          text: '<i class="fa fa-file-pdf"></i>',
+          titleAttr: 'PDF'
+        },
+        // {
+        //   extend: 'print',
+        //   text: '<i class="fa fa-print"></i>',
+        //   titleAttr: 'Print'
+        // },
+      ],
       "order": [
         [0, "desc"],
       ],
